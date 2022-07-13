4d54d205dd75efea4d95a79976875ba69326511d
DeviousLab <deviouslab@gmail.com>
2022-07-14 03:21:47 +0400

    Bug fixing for heroku deployment
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import queryString from 'query-string';
 
-const baseURL = 'http://localhost:5000/api/v1' || process.env.REACT_APP_API_URL;
+const baseURL = '/api/v1' || process.env.REACT_APP_API_URL;
 const getToken = () => localStorage.getItem('token');
 
 const axiosClient = axios.create({
